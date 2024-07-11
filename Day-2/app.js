07b0544297d4b66ea785a88031bfcc01a7db64d1
shashikant884 <yadavshashikant884@gmail.com>
2024-07-11 11:30:02 +0530

    bug fixed

--- Day-2/app.js
+++ Day-2/app.js
@@ -7,17 +7,39 @@ import ReactDOM from "react-dom/client";
 // root.render(heading);
 
 // ReactElement(object) => hrml(browser understands)
-const parent = React.createElement(
-    "div" , {id : "parent"},
-    React.createElement("div" , {id : "child"},
-        [React.createElement("h1" , {}, "Hi am from H1 Tag iam saving this file"),
-            React.createElement("h1" , {}, "Hi am from H2 Tag")
-        ]    
-    )
+// const parent = React.createElement(
+//     "div" , {id : "parent"},
+//     React.createElement("div" , {id : "child"},
+//         [React.createElement("h1" , {}, "Hi am from H1 Tag iam saving this file"),
+//             React.createElement("h1" , {}, "Hi am from H2 Tag")
+//         ]    
+//     )
+// );
+
+// console.log(parent);
+
+
+
+// const jsxHeading = <h1 id="heading">Namaste React using jsx</h1>;
+const Title = ()=>(
+    <h1 className="container">
+        Hello React using jsx
+    </h1>
 );
 
-console.log(parent);
+const HeadingComponent = ()=>(
+    <div id="container">
+        <Title/>
+        <h1>React functional HeadingComponent</h1>
+    </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent); 
+
+// console.log(jsxHeading);
+
+
+
+// root.render(parent); 
+root.render(<HeadingComponent/>);
