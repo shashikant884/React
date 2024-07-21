41f683842b02116e00c67cf379a1a411c70a1561
shashikant884 <yadavshashikant884@gmail.com>
2024-07-21 23:23:40 +0530

    Error page

--- Day-2/src/App.js
+++ Day-2/src/App.js
@@ -3,6 +3,8 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
 import {createBrowserRouter , RouterProvider} from "react-router-dom"
 
  
@@ -16,13 +18,19 @@ const AppLayout = ()=>(
 const appRouter = createBrowserRouter([
     {
         path : "/",
-        element : <AppLayout />
+        element : <AppLayout />,
+        errorElement : <Error />
     },
     {
         path : "/about",
-        element : <About />
+        errorElement : <About />
+    },
+    {
+        path : "/contact",
+        element : <Contact />
     },
 ]);
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
---
+++ Day-2/src/components/Contact.js
@@ -0,0 +1,12 @@
+
+
+const Contact = (()=>{
+    return(
+        <div>
+            <h3>Hello i am from Contact page</h3>
+        </div>
+        
+    )
+});
+
+export default Contact;
