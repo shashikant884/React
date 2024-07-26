06ea16cdabc888381ebd453ac062b2a90bc8632e
shashikant884 <yadavshashikant884@gmail.com>
2024-07-26 14:44:05 +0530

    props

--- Day-2/src/components/User.js
+++ Day-2/src/components/User.js
@@ -1,8 +1,20 @@
+import { useState } from "react";
+
 const User = (props)=>{
+    const [count , setCount] = useState(0);
+    const count1 =useState(1);
+
     
     return(
         <div className="user-card">
             <h3>Name : {props.name}</h3>
+            <h4>{count}</h4>
+            <button
+            onClick={()=>{
+                setCount(count+1)
+            }}
+            >click me!</button>
+            <h4>{count1}</h4>
             <h3>Location : Mumbai</h3>
             <p>contacts : @shashikant884</p>        
         </div>
--- Day-2/src/components/UserClass.js
+++ Day-2/src/components/UserClass.js
@@ -4,13 +4,32 @@ class UserClass extends React.Component{
     constructor(props){
         super(props);
         console.log(props);
-    }
+
+        this.state = {
+            count: 0,
+            count1: 1
+        };
+        console.log("In side Constructor..")
+    } 
 
     render(){
+        console.log("In side rende..")
+
         const {name } = this.props;
+        const {count , count1}  = this.state;                                 
         return(
             <div className="user-card">
+
             <h3>Name : {name}</h3>
+            <h4>{count}</h4>
+            <button
+                onClick={()=>{
+                    this.setState({
+                        count : this.state.count+1,
+                    });
+                }}
+            >Click me to Increase</button>
+            <h4>{count1}</h4>
             <h3>Location : Mumbai</h3>
             <p>contacts : @shashikant884</p>        
         </div>
